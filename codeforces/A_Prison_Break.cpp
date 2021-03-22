345cc3127b75206d331120c04ad0934d7764b4c9
Vinicius Agostini <vinicius.agostini19@gmail.com>
2021-03-21 22:26:18 -0300

    cf round 709

---
+++ codeforces/A_Prison_Break.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    for (int tc = 1; tc <= t; tc++) {
+        int a, b;
+        cin >> a >> b;
+        cout << a * b << endl;
+    }
+}
---
+++ codeforces/C_Basic_Diplomacy.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    for (int tc = 1; tc <= t; tc++) {
+        int n, m;
+        cin >> n >> m;
+
+        int limit = (m % 2) ? (m + 1) / 2 : m / 2;
+
+        vector<int> ans(m);
+        vector<int> freq(n + 1, 0);
+        vector<pair<int, set<int>>> d(m);
+
+        for (int i = 0; i < m; i++) {
+            int k, f;
+            cin >> k;
+            d[i] = {i, {}};
+            for (int j = 0; j < k; j++) {
+                cin >> f;
+                d[i].second.insert(f);
+            }
+        }
+
+        sort(d.begin(), d.end(), [&](pair<int, set<int>> a, pair<int, set<int>> b) {
+            return a.second.size() < b.second.size();
+        });
+
+        // for (auto p : d) {
+        //     cout << "i: " << p.first << " f: ";
+        //     for (auto fi : p.second) {
+        //         cout << fi << " ";
+        //     }
+        //     cout << endl;
+        // }
+
+        bool possible = true;
+        // greedily pick friends from each day
+        for (auto p : d) {
+            for (auto fi : p.second) {
+                if (freq[fi] < limit) {
+                    freq[fi]++;
+                    ans[p.first] = fi;
+                    break;
+                }
+            }
+            if (!ans[p.first]) {
+                possible = false;
+                cout << "NO" << endl;
+                break;
+            }
+        }
+
+        if (possible) {
+            cout << "YES" << endl;
+            for (int i = 0; i < m; i++) {
+                cout << ans[i] << " ";
+            }
+            cout << endl;
+        }
+    }
+}
